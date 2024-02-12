4e6b5d68e591373864e17039844fe5e484e724b6
Subhash Jha <subhashjha@MacBook-Pro.fritz.box>
2024-02-12 22:23:03 +0100

    feat: featureAvailable flag added to disable unimplemented feature button

--- src/app/components/assitant-card/assitant-card.component.ts
+++ src/app/components/assitant-card/assitant-card.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-assitant-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatTooltipModule],
   templateUrl: './assitant-card.component.html',
   styleUrl: './assitant-card.component.scss'
 })
 export class AssitantCardComponent {
   @Input() title = '';
   @Input() content = '';
+  @Input() featureAvailable: boolean = true;
+
 }
