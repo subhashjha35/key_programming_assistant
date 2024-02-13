740b8fbd210f4ef220bb1cb0826444edb65d6ebe
Subhash Jha <subhashjha@MacBook-Pro.local>
2024-02-13 09:38:55 +0100

    test: key replacement service is tested with rxjs-marbles

--- src/app/services/key-replacement.service.spec.ts
+++ src/app/services/key-replacement.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { firstValueFrom } from 'rxjs';
+import { marbles } from 'rxjs-marbles';
 import { KeyReplacementService } from './key-replacement.service';
 
 describe('KeyReplacementService', () => {
@@ -12,14 +12,19 @@ describe('KeyReplacementService', () => {
     service = TestBed.inject(KeyReplacementService);
   });
 
-  it('should invalidate device after 5 time units', async () => {
+  it('should invalidate device after 5 time units', marbles(async m => {
+    const expectedMarble = '5s a|';
+    const expectedValue = { a: true };
 
-      const result = await firstValueFrom(service.invalidateDevice());
-      expect(result).toEqual(true);
-  });
+    const result$ = await service.invalidateDevice();
+    m.expect(result$).toBeObservable(m.cold(expectedMarble, expectedValue));
+  }));
 
-  it('should program device after 3 seconds', async () => {
-    const result = await firstValueFrom(service.programDevice());
-      expect(result).toEqual(true);
-  });
+  it('should program device after 3 seconds', marbles(async m => {
+    const expectedMarble = '3s a|';
+    const expectedValue = { a: true };
+
+    const result$ = await service.programDevice();
+    m.expect(result$).toBeObservable(m.cold(expectedMarble, expectedValue));
+  }));
 });
